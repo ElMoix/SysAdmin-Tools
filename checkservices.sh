696c24ed17150958f69b23cf3b069017c6913da7
ElMoix <23andreu@gmail.com>
2023-05-11 13:22:26 +0200

    final checkservices

--- checkservices.sh
+++ checkservices.sh
@@ -3,23 +3,23 @@
 # Cron:
 # */30 * * * * /root/scripts/checkservices.sh
 
-services=("docker" "ssh" "openvpn" "rsyslog" "cron" "apache")
+services=("docker" "ssh" "openvpn" "rsyslog" "cron" "apache2")
 
 for service in ${services[@]}; do
         health=$(systemctl is-active $service)
         if [ $health == "active" ];then
-                echo "$(date) - $service is running on $(hostname)" >> /var/log/andreu/services.log
+                echo "$(date) - $service is running on $(hostname)" >> /var/log/moix/services.log
                 exit 0
         elif [ $health == "inactive" ];then
-                echo "$(date) - $service is not running $(hostname)" >> /var/log/andreu/services.log
+                echo "$(date) - $service is not running $(hostname)" >> /var/log/moix/services.log
                 mail -s "SERVICE $service STOPPED on $(hostname)" 23andreu@gmail.com
                 exit 0
         elif [ $health == "failed" ];then
-                echo "$(date) - !!ERROR!! $service can't be started. Errors must be corrected on $(hostname) !!" >> /var/log/andreu/services.log
+                echo "$(date) - !!ERROR!! $service can't be started. Errors must be corrected on $(hostname) !!" >> /var/log/moix/services.log
                 mail -s "ERROR!! SERVICE $service FAILED on $(hostname)" 23andreu@gmail.com
                 exit 0
         else
-                echo "$(date) - !!WARNING!! $service can't be started. Check $service on $(hostname) !!" >> /var/log/andreu/services.log
+                echo "$(date) - !!WARNING!! $service can't be started. Check $service on $(hostname) !!" >> /var/log/moix/services.log
                 mail -s "WARNING!! SERVICE $service FAILED on $(hostname)" 23andreu@gmail.com
                 exit 1
         fi
