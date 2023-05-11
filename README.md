c48d6a83132ee6dea4f7952231a238fad8a3268b
ElMoix <23andreu@gmail.com>
2023-05-11 13:36:15 +0200

    final beforeshutdown

--- README.md
+++ README.md
@@ -13,8 +13,7 @@ it's a script that makes 5 pings to 8.8.8.8 and 1.1.1.1 and save it to log file.
 ```
 it's a script that checks the status of every service running on the machine and sends an email
 ```
-# beforeshutdown.sh
+## beforeshutdown.sh
 ```
 it's a script that sends an email when the machine goes shutdown.
-You must have to execute the pre-beforeshutdown.sh script.
 ```
--- beforeshutdown.sh
+++ beforeshutdown.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
+
+file=/etc/systemd/system/run-before-shutdown.service
+if [ ! -f "$file" ];then
+echo "[Unit]
+Description=Run my custom task at shutdown
+DefaultDependencies=no
+Before=shutdown.target
+
+[Service]
+Type=oneshot
+ExecStart=/root/scripts/beforeshutdown.sh
+TimeoutStartSec=0
+
+[Install]
+WantedBy=shutdown.target" >> $file
+fi
+
 echo "$(date) - $(hostname) Shutdown" >> /var/log/moix/shutdown.log
 mail -s "$(hostname) Shutdown" 23andreu@gmail.com
